9229000eab3e9c479e09bfed105f2de48bf2f410
Ahmed Yousri <ayousry42@gmail.com>
2024-10-20 02:53:31 +0300

    Finished SignUp (only as interface)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes , Route } from "react-router-dom"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/HomePage"
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path = '/' element = {<HomePage/>} />
+      <Route path = '/login' element = {<LoginPage/>} />
+      <Route path = '/signup' element = {<SignUpPage/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='hero-bg h-screen'>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage

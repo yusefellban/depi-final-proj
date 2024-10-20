9d77e6b5f169406a2a2b6287333a0c000ec79411
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-10-20 15:22:28 +0300

    Done

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,10 +1,19 @@
-import express from 'express';
-import { getTrendingMovies } from '../controllers/movie.controller.js';
+import express from "express";
+import { getTrendingMovies } from "../controllers/movie.controller.js";
+import { getMovieTrailers } from "../controllers/movie.controller.js";
+import { getMovieDetails } from "../controllers/movie.controller.js";
+import { getSimilarMovies } from "../controllers/movie.controller.js";
+import { getMoviesByCategory } from "../controllers/movie.controller.js";
+import { getMoviesByTopRated } from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovies);
-router.get("/popular", getPopularMovies);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
+router.get("/:category", getMoviesByCategory);
+router.get("/:top_rated", getMoviesByTopRated);
 
 
-export default router;  
+export default router;
--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
-import { ENV_VARS } from '../config/envVars.js';
+import axios from "axios";
+import { ENV_VARS } from "../config/envVars.js";
 
 // fetch(url, options)
 //   .then((res) => res.json())
 //   .then((json) => console.log(json))
 //   .catch((err) => console.error("error:" + err));
 
-export const fetchFromTMDB = async(url) => {
+export const fetchFromTMDB = async (url) => {
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer" + ENV_VARS.TMDB_API_KEY,
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMjM1MDk4ZWJmMzM2ZmJlY2UzMmRkZDIwN2VkYzQ0NiIsIm5iZiI6MTcyOTQxMzY4NS4wOTM5OTYsInN1YiI6IjY3MTRjMDM1OTlmMjJmMzI2YWFkNjNkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zurm9NP8opRZfFHLthNfn4ld3hMdgi5mJiYRDZx49rU",
     },
   };
-  const res = await axios.get(url, options)
-  if(res.status !== 200){
-    throw new Error('Failed to fetch data from TMDB' + res.statusText);
+  const res = await axios.get(url, options);
+  if (res.status !== 200) {
+    throw new Error("Failed to fetch data from TMDB" + res.statusText);
   }
   return res.data;
-}
+};
